7d6c5cb5efb5658c61c236c7d100c520f7f8295b
kj2whvbzjn-hue <kj2whvbzjn@privaterelay.appleid.com>
2026-09-18 12:27:19 +0000

    Issue #90: export policy validation helper

--- productization_v1/__init__.py
+++ productization_v1/__init__.py
@@ -1,22 +1,3 @@
 """Public Productization v1 core package surface."""
-from .core import (
-    PrincipalGrant,
-    canonical_json,
-    deterministic_identity,
-    event_is_authorized,
-    package_manifest,
-    plan_single_repo_reconcile,
-    resolve_role,
-    validate_persisted_input,
-)
-
-__all__ = [
-    "PrincipalGrant",
-    "canonical_json",
-    "deterministic_identity",
-    "event_is_authorized",
-    "package_manifest",
-    "plan_single_repo_reconcile",
-    "resolve_role",
-    "validate_persisted_input",
-]
+from .core import PrincipalGrant,canonical_json,deterministic_identity,event_is_authorized,package_manifest,plan_single_repo_reconcile,resolve_role,validate_authorization_policy,validate_persisted_input
+__all__=["PrincipalGrant","canonical_json","deterministic_identity","event_is_authorized","package_manifest","plan_single_repo_reconcile","resolve_role","validate_authorization_policy","validate_persisted_input"]
