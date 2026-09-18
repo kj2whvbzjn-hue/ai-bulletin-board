41b4c96740a5f996ed751b7af3796d60554337b5
kj2whvbzjn-hue <kj2whvbzjn@privaterelay.appleid.com>
2026-09-18 21:23:43 +0000

    Export Phase-2A operator projection

--- productization_v1/__init__.py
+++ productization_v1/__init__.py
@@ -1,3 +1,34 @@
 """Public Productization v1 core package surface."""
-from .core import PrincipalGrant,canonical_json,deterministic_identity,event_is_authorized,package_manifest,plan_single_repo_reconcile,resolve_role,validate_authorization_policy,validate_persisted_input
-__all__=["PrincipalGrant","canonical_json","deterministic_identity","event_is_authorized","package_manifest","plan_single_repo_reconcile","resolve_role","validate_authorization_policy","validate_persisted_input"]
+from .core import (
+    PrincipalGrant,
+    canonical_json,
+    deterministic_identity,
+    event_is_authorized,
+    package_manifest,
+    plan_single_repo_reconcile,
+    resolve_role,
+    validate_authorization_policy,
+    validate_persisted_input,
+)
+from .operator_projection import (
+    EVIDENCE_FIELDS,
+    HUMAN_REQUIRED_FIELDS,
+    OPERATOR_STATES,
+    project_operator_state,
+)
+
+__all__ = [
+    "PrincipalGrant",
+    "canonical_json",
+    "deterministic_identity",
+    "event_is_authorized",
+    "package_manifest",
+    "plan_single_repo_reconcile",
+    "resolve_role",
+    "validate_authorization_policy",
+    "validate_persisted_input",
+    "EVIDENCE_FIELDS",
+    "HUMAN_REQUIRED_FIELDS",
+    "OPERATOR_STATES",
+    "project_operator_state",
+]
